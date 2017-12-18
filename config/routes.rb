1c880e2f9d2b6de3be38f0304798e43c5482acb6
Douglas Campbell <dacampbelltbone@gmail.com>
2017-12-17 23:53:39 -0500

    add styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :subcategories
   resources :categories
   resources :listings
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'categories#index'
 end
